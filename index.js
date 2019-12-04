c5da5b96282c179ee05b5628a7b5bece755f5184
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-12-04 13:19:40 +0300

    Display todos in the console

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * This is a little Todo app that I created with the practical javascript course.
+ * Remember that everything in life is really just one big todo!
+ */
+
+let todos = ['item1', 'item2', 'item3'];
+console.log('My Todos :', todos);
